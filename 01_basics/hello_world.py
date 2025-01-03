0ed1e0f0bd53463546684945c938c0f2113a913c
Roshan Khetpal <roshankhetpalofficial@gmail.com>
2025-01-03 22:02:47 +0530

    Basic module passing and hello world printing

---
+++ 01_basics/hello_world.py
@@ -0,0 +1,22 @@
+# In this program, we will print "hello world" to the console
+# Print is a built in function in python that prints the given string to the console
+# To run this program, open the terminal and type "python hello_world.py" and press enter
+
+print("hello world")
+
+# Output:
+# hello world
+
+##################################
+
+# in pyton, we can define a funtion using the keyword "def".
+# The syntax to define a function is:
+# def function_name(parameters):
+#     funciton_body
+#   return value
+# To call a fuction, we use the function name followed by the paranthesis and the parameters
+
+def color(m):
+    print(m)
+
+color("Green")
---
+++ 01_basics/method_passing.py
@@ -0,0 +1,6 @@
+# This is a simple example of passing a method from one module to another module.
+# In this example, we are passing the color method from hello_world.py to method_passing.py
+# Bascally importing the color method
+from hello_world import color
+
+color("blue") # Output: blue
